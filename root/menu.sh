efca56678ff0ee8ccaef6522c65c3ebc22d5af4a
oilervoss <euler(a)alves(.)pro(.)br>
2017-11-10 17:27:09 -0200

    Fixed menu

--- root/menu.sh
+++ root/menu.sh
@@ -13,10 +13,7 @@ while true; do
 	ARIA=$( ckser aria2c )
 	TRANSMISSION=$( ckser transmission-daemon )
 	OPENVPN=$( ckser openvpn )
-<<<<<<< HEAD
 	CHOICE=""	
-=======
->>>>>>> e5507189d23ad074f98b2deef7cbcfe5415927d2
 	clear
 	if $ARIA; then 
 		echo -ne "[*]=<A>ria stop\n"; 
@@ -39,19 +36,12 @@ while true; do
 		echo -ne "    <C>hange Nordvpn: [";
 
 	ls -l /etc/openvpn/nordvpn.ovpn | sed -r 's:^.+-> .*nordvpn/(.+).ovpn$:\1]:'
-<<<<<<< HEAD
 	if $OPENVPN; then
 		echo -ne "    <K>ill Openvpn\n";
 	fi
 	echo -ne "    <Q>uit to shell\n"
 
 	read -t 3 -n 1 CHOICE
-=======
-
-	echo -ne "    <S>hell\n"
-
-	read -sn 1 CHOICE
->>>>>>> e5507189d23ad074f98b2deef7cbcfe5415927d2
 
 	case $CHOICE in
 		A|a )
@@ -75,7 +65,6 @@ while true; do
 			fi
 			/etc/init.d/openvpn start
 			;;
-<<<<<<< HEAD
 		K|k )
 			rm -f /etc/openvpn/openvpn.lock
 			/etc/init.d/openvpn stop
@@ -84,19 +73,8 @@ while true; do
 			/root/nordvpn.sh
 			;;
 		Q|q )
-=======
-		C|c )
-			/root/nordvpn.sh
-			;;
-		S|s )
->>>>>>> e5507189d23ad074f98b2deef7cbcfe5415927d2
 			clear
 			exit 0
 			;;
 	esac
 done
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e5507189d23ad074f98b2deef7cbcfe5415927d2
